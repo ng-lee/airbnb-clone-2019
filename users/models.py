7fb7a75f1fe9f20a9da8d7d986ebb9f17db34d87
codeer-kr <65810091+codeer-kr@users.noreply.github.com>
2021-10-05 16:47:12 +0900

    Customed User Admin
    list_display and list_filter

--- users/models.py
+++ users/models.py
@@ -19,19 +19,12 @@ class User(AbstractUser):
     LANGUAGE_ENG = "eng"
     LANGUAGE_KOR = "kor"
 
-    LANGUAGE_CHOICES = (
-        (LANGUAGE_ENG, "English"),
-        (LANGUAGE_KOR, "Korean")
-    )
+    LANGUAGE_CHOICES = ((LANGUAGE_ENG, "English"), (LANGUAGE_KOR, "Korean"))
 
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
 
-    CURRENCY_CHOICES = (
-        (CURRENCY_USD, "USD"),
-        (CURRENCY_KRW, "KRW")
-    )
-
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_KRW, "KRW"))
 
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
@@ -39,6 +32,10 @@ class User(AbstractUser):
     )
     bio = models.TextField(default="", blank=True)
     birthdate = models.DateField(null=True, blank=True)
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=3, null=True, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=3, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, null=True, blank=True
+    )
     superhost = models.BooleanField(default=False)
