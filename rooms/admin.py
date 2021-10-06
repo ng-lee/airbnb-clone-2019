acc6c5f03deab9433f06204e311bcd8f398cee77
codeer-kr <65810091+codeer-kr@users.noreply.github.com>
2021-10-06 15:47:20 +0900

    Created AbstractItem and RoomType

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from . import models
 
+
+@admin.register(models.RoomType)
+class RoomTypeAdmin(admin.ModelAdmin):
+
+    pass
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
     pass
-
--- rooms/models.py
+++ rooms/models.py
@@ -4,9 +4,30 @@ from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
 
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """Abstract Item"""
+
+    name = models.CharField(max_length=140)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+
+    """Room Type Definition"""
+
+    pass
+
+
 class Room(core_models.TimeStampedModel):
 
-    """ Room Model Definition """
+    """Room Model Definition"""
 
     name = models.CharField(max_length=140)
     description = models.TextField()
@@ -22,4 +43,7 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(user_models.User, on_delete=CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
 
+    def __str__(self):
+        return self.name
