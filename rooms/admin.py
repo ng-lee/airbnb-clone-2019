8d8382c1cf62e0ad87c272586a4c220311c11821
codeer-kr <65810091+codeer-kr@users.noreply.github.com>
2021-10-11 00:31:32 +0900

    Added Admin of Photo in Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -14,10 +15,16 @@ class ItemAdmin(admin.ModelAdmin):
         return obj.rooms.count()
 
 
+class PhotoInline(admin.TabularInline):
+    model = models.Photo
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
+
+    inlines = (PhotoInline,)
 
     fieldsets = (
         (
@@ -40,6 +47,7 @@ class RoomAdmin(admin.ModelAdmin):
             "More about the Space",
             {"fields": ("amenities", "facilities", "house_rules")},
         ),
+        ("Last Details", {"fields": ("host",)}),
     )
 
     list_display = (
@@ -58,6 +66,8 @@ class RoomAdmin(admin.ModelAdmin):
         "count_amenities",
         "count_photos",
     )
+
+    raw_id_fields = ("host",)
 
     ordering = ("name", "price")
 
@@ -86,4 +96,9 @@ class RoomAdmin(admin.ModelAdmin):
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
-    pass
+    """Photo Admin Definition"""
+
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="50px" src={obj.file.url} />')
