dc4fe68b38ed66a80cd5ae4baadd444db88957e7
codeer-kr <65810091+codeer-kr@users.noreply.github.com>
2021-10-10 15:41:30 +0900

    Updated related_name for all models

--- reservations/models.py
+++ reservations/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from core import models as core_models
 
+
 class Reservation(core_models.TimeStampedModel):
-    
-    """ Reservation Model Definition """
+
+    """Reservation Model Definition"""
 
     STATUS_PENDING = "pending"
     STATUS_CONFIRMED = "comfirmed"
@@ -16,11 +17,18 @@ class Reservation(core_models.TimeStampedModel):
         (STATUS_CANCELED, "Canceled"),
     )
 
-    status = models.CharField(max_length=10, choices=STATUS_CHOICE, default=STATUS_PENDING)
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICE, default=STATUS_PENDING
+    )
     check_in = models.DateField()
     check_out = models.DateField()
-    guest = models.ForeignKey("users.User", on_delete=CASCADE)
-    room = models.ForeignKey("rooms.Room", on_delete=CASCADE)
+
+    guest = models.ForeignKey(
+        "users.User", related_name="reservations", on_delete=CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reservations", on_delete=CASCADE
+    )
 
     def __str__(self):
         return f"{self.room} - {self.check_in}"
--- rooms/models.py
+++ rooms/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.db.models.deletion import CASCADE, SET_NULL
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as user_models
 
 
 class AbstractItem(core_models.TimeStampedModel):
@@ -56,7 +55,8 @@ class Photo(core_models.TimeStampedModel):
 
     caption = models.CharField(max_length=80)
     file = models.ImageField()
-    room = models.ForeignKey("Room", on_delete=CASCADE)
+
+    room = models.ForeignKey("Room", related_name="photos", on_delete=CASCADE)
 
     def __str__(self):
         return self.caption
@@ -79,11 +79,15 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey(user_models.User, on_delete=CASCADE)
-    room_type = models.ForeignKey(RoomType, on_delete=SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity, blank=True)
-    facilities = models.ManyToManyField(Facility, blank=True)
-    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    host = models.ForeignKey("users.User", related_name="rooms", on_delete=CASCADE)
+    room_type = models.ForeignKey(
+        "RoomType", related_name="rooms", on_delete=SET_NULL, null=True
+    )
+
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     def __str__(self):
         return self.name
