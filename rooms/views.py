78b679d929115fc6dd6e1947c0ae6bdda8ba1ea0
codeer-kr <65810091+codeer-kr@users.noreply.github.com>
2021-10-22 00:43:59 +0900

    Replace with paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,22 +1,15 @@
-from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
-    page_size = 10
-    limit = page_size * page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
-    page_count = ceil(models.Room.objects.count() / page_size)
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10, orphans=5)
+    rooms = paginator.get_page(page)
     return render(
         request,
         "rooms/home.html",
-        {
-            "potato": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),
-        },
+        {"page": rooms},
     )
