a62bf4f2cd9ebb406fae7a0b3af10cae97092d62
codeer-kr <65810091+codeer-kr@users.noreply.github.com>
2021-11-01 15:17:26 +0900

    Created Sign Up Validation

--- users/views.py
+++ users/views.py
@@ -23,3 +23,10 @@ class LoginView(FormView):
 def log_out(request):
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
