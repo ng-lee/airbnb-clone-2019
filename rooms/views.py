fc1515afa2e40333a438111c98bdc5a1463999d5
codeer-kr <65810091+codeer-kr@users.noreply.github.com>
2021-11-07 21:12:13 +0900

    Designed Photo of Room Detail

--- rooms/views.py
+++ rooms/views.py
@@ -24,7 +24,7 @@ class HomeView(ListView):
 def room_detail(request, pk):
     try:
         room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
+        return render(request, "rooms/room_detail.html", {"room": room})
     except models.Room.DoesNotExist:
         raise Http404()
 
