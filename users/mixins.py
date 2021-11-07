c09ba3600fabf30fc716963cfefd3c093f452c3b
codeer-kr <65810091+codeer-kr@users.noreply.github.com>
2021-11-07 20:51:04 +0900

    Done with USERS

--- users/mixins.py
+++ users/mixins.py
@@ -1,13 +1,28 @@
+from django.contrib import messages
 from django.shortcuts import redirect, reverse
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.urls.base import reverse_lazy
+from . import models
+
+
+class EmailLoginOnlyView(LoginRequiredMixin):
+    def test_func(self):
+        return self.request.user.login_method == models.User.LOGIN_EMAIL
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Can't go there!")
+        return redirect(reverse("core:home"))
 
 
 class LoggedOutOnlyView(UserPassesTestMixin):
-
-    permission_denied_message = "Page not found"
-
     def test_func(self):
         return not self.request.user.is_authenticated
 
     def handle_no_permission(self):
+        messages.error(self.request, "Can't go there!")
         return redirect(reverse("core:home"))
+
+
+class LoggedInOnlyView(LoginRequiredMixin):
+
+    login_url = reverse_lazy("users:login")
--- users/views.py
+++ users/views.py
@@ -14,7 +14,6 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = "users/login.html"
     form_class = forms.LoginForm
-    success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -23,6 +22,13 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
         if user is not None:
             login(self.request, user)
         return super().form_valid(form)
+
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
 
 
 def log_out(request):
@@ -201,7 +207,7 @@ class UserProfileView(DetailView):
     context_object_name = "user_obj"
 
 
-class UpdateProfileView(UpdateView):
+class UpdateProfileView(mixins.LoggedInOnlyView, UpdateView):
 
     model = models.User
     template_name = "users/update-profile.html"
@@ -234,7 +240,9 @@ class UpdateProfileView(UpdateView):
         return form
 
 
-class UpdatePasswordView(PasswordChangeView):
+class UpdatePasswordView(
+    mixins.LoggedInOnlyView, mixins.EmailLoginOnlyView, PasswordChangeView
+):
 
     template_name = "users/update-password.html"
 
